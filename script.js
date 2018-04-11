367d985a6f574cfd0a025309965fc078c0358def
TheCodeWorm <4purposeliving@gmail.com>
2018-04-11 05:08:21 -0700

    added button to clear all from list

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-var shoppingList = [];
+function toggleDone() {
+	if (this.className == "done listItem")
+		this.className = "listItem";
+	else this.className = "done listItem";
+}
 
-function removeFromListElement() {
-	this.parentNode.parentNode.removeChild(this.parentNode);
+function clearStrikes() { 
+	var iList = document.getElementById("itemsList").getElementsByTagName("li");
+	for (var i=iList.length-1; i>=0; i--) {
+	  if (iList[i].firstChild.className === "done listItem") {
+			  iList[i].parentNode.removeChild(iList[i]);  	
+		}
+	}
+}
+
+function clearList() {
+	var iList = document.getElementById("itemsList").getElementsByTagName("li");
+	for (var i=iList.length-1; i >= 0; i--) {
+	  iList[i].parentNode.removeChild(iList[i]);  
+	}
 }
 
 function addToListElement() {
 	var newButton = document.createElement("BUTTON");
 	newButton.appendChild(document.createTextNode(input.value));
-	newButton.addEventListener("click", removeFromListElement);
+	newButton.addEventListener("click", toggleDone);
 	newButton.className = "listItem";
 	ul.appendChild(document.createElement("li")).appendChild(newButton);
 	input.value = "";
@@ -25,15 +41,11 @@ function addListAfterClick() {
 
 function addListAfterKeypress(event) {
 	if (input.value.length > 0 && event.keyCode === 13) {
-		var shoppingItem = {
-			id: shoppingList.length,
-	    name: input.value,
-	    onStatus: false 
-    }
-    shoppingList.push(shoppingItem);
-		addToListElement(shoppingItem);
+		addToListElement();
 	}
 }
 
-button.addEventListener("click", addListAfterClick);
+document.getElementById("clearAll").addEventListener("click", clearList);
+document.getElementById("enter").addEventListener("click", addListAfterClick);
+document.getElementById("clear").addEventListener("click", clearStrikes);
 input.addEventListener("keypress", addListAfterKeypress);
