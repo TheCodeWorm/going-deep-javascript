7e61103bd8e7ada7e1e1d521ebafa8e3dfbb2414
TheCodeWorm <4purposeliving@gmail.com>
2018-04-12 12:02:14 -0700

    update

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // For testing....
 
 
-var arr1 = ['Leslie', 'Carrie', 'Donna']
-var str = ""
-arr1.forEach( function (e) { str += (e+' ') });
-
+var nameArray = ['Leslie', 'Carrie', 'Donna']
+//arr1.forEach( function (e) { str += (e+' ') });
+function myFunction(arr) {
+	var str = "";
+	for (var i=0; i<arr.length; i++) {
+		str += ( arr[i] + ' ' );
+	}
+	return str;
+};
+var str = myFunction(nameArray);
+console.log(str);
 var h = document.createElement("H1");
 var t = document.createTextNode(str);
 h.appendChild(t);
